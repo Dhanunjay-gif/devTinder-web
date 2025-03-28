5d77778d75c0854aa659db77030dfc2f33162648
dhanu <boyadhanunjay01@gmail.com>
2025-03-28 22:12:50 +0530

    new

--- src/components/ConnectionRequestCard.jsx
+++ src/components/ConnectionRequestCard.jsx
@@ -4,56 +4,37 @@ const ConnectionRequestCard = ({ user }) => {
   const { firstName, lastName, age, about, gender, photourl, skills } = user;
 
   return (
-    <div className="flex flex-col items-center p-6">
-      <div className="text-center text-4xl mb-6">Connections</div>
+    <tr className="border-b border-gray-300 hover:cursor-pointer">
+      {/* Name and Profile */}
+      <td className="p-4 flex items-center gap-4">
+        <div className="h-12 w-12 flex-shrink-0 rounded-full overflow-hidden">
+          <img src={photourl} alt="User" className="h-full w-full object-cover" />
+        </div>
+        <div>
+          <div className="font-semibold">{firstName} {lastName}</div>
+          <div className="text-sm opacity-50">India</div>
+        </div>
+      </td>
 
-      <div className="w-full max-w-4xl overflow-x-auto">
-        <table className="table-auto w-full border-collapse border border-gray-300">
-          {/* Table Header */}
-          <thead>
-            <tr className="text-left border-b border-gray-300">
-              <th className="p-3">Name</th>
-              <th className="p-3">Gender</th>
-              <th className="p-3">Skills</th>
-              <th className="p-3">About</th>
-            </tr>
-          </thead>
+      {/* Gender and Age */}
+      <td className="p-4 text-center">
+        {gender}
+        <br />
+        <span className="text-sm opacity-50">Age: {age}</span>
+      </td>
 
-          {/* Table Body */}
-          <tbody>
-            <tr className="border-b border-gray-300">
-              {/* Name and Profile */}
-              <td className="p-3 flex items-center gap-3">
-                <div className="h-12 w-12 flex-shrink-0 rounded-full overflow-hidden">
-                  <img src={photourl} alt="User" className="h-full w-full object-cover" />
-                </div>
-                <div>
-                  <div className="font-semibold">{firstName} {lastName}</div>
-                  <div className="text-sm opacity-50">India</div>
-                </div>
-              </td>
+      {/* Skills */}
+      <td className="p-4 text-center">
+        {skills && skills.length > 0 ? skills.join(", ") : "N/A"}
+      </td>
 
-              {/* Gender and Age */}
-              <td className="p-3">
-                {gender}
-                <br />
-                <span className="text-sm opacity-50">Age: {age}</span>
-              </td>
-
-              {/* Skills */}
-              <td className="p-3">{skills.join(", ")}</td>
-
-              {/* About Button */}
-              <td className="p-3">
-                <button className="border border-gray-400 px-4 py-1 rounded-md text-sm">
-                  {about}
-                </button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
+      {/* About Button */}
+      <td className="p-4 text-center">
+        <button className="px-4 py-2 rounded-lg border border-gray-400 hover:cursor-pointer">
+          {about || "No details available"}
+        </button>
+      </td>
+    </tr>
   );
 };
 
--- src/components/Connections.jsx
+++ src/components/Connections.jsx
@@ -1,44 +1,60 @@
-import React, { useEffect, useState } from 'react'
-import { BASE_URL } from '../utils/constants'
-import axios from 'axios'
-import { useDispatch, useSelector } from 'react-redux'
-import { addConnection } from '../utils/connectionSlice'
-import ConnectionRequestCard from './ConnectionRequestCard'
+import React, { useEffect } from 'react';
+import { BASE_URL } from '../utils/constants';
+import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
+import { addConnection } from '../utils/connectionSlice';
+import ConnectionRequestCard from './ConnectionRequestCard';
 
 const Connections = () => {
     const dispatch = useDispatch();
-    const connectionData =useSelector(store=>store.connection)
-    const fetchConnection =  async ()=>{
-        if(connectionData?.length) return
-        try{
-            const res = await axios.get(BASE_URL+"/user/connections",{
-                withCredentials:true
-            })
-            dispatch(addConnection(res?.data?.data || []))
-        }
-        catch (err) {
-            console.error("Error finding connection:", err);
-        }
-    }
+    const connectionData = useSelector(store => store.connection);
 
-    useEffect(()=>{
+    const fetchConnection = async () => {
+        if (connectionData?.length) return;
+        try {
+            const res = await axios.get(BASE_URL + "/user/connections", {
+                withCredentials: true
+            });
+            dispatch(addConnection(res?.data?.data || []));
+        } catch (err) {
+            console.error("Error fetching connections:", err);
+        }
+    };
+
+    useEffect(() => {
         fetchConnection();
-    },[])
+    }, []);
 
-  return (
-    <div>
-        {
-            connectionData && connectionData?.map((connection)=>{
-                const {firstName,lastName,age,about,gender,photourl,_id,skills} = connection;
-                return (
-                    <div key={_id}>
-                        <ConnectionRequestCard user={{firstName,lastName,age,about,gender,photourl,_id,skills}}/>
-                    </div>
-                )
-            })
-        }
-    </div>
-  )
-}
+    return (
+        <div className="p-6">
+            <h1 className="text-center text-4xl font-semibold mb-6">My Connections</h1>
 
-export default Connections
+            {connectionData?.length === 0 ? (
+                <p className="text-center text-gray-500">No Connections Found</p>
+            ) : (
+                <div className="w-full max-w-4xl mx-auto overflow-x-auto">
+                    <table className="table-auto w-full min-w-[600px] border-collapse border border-gray-300">
+                        {/* Table Header */}
+                        <thead>
+                            <tr className="border-b border-gray-300">
+                                <th className="p-4 text-center">Name</th>
+                                <th className="p-4 text-center">Gender</th>
+                                <th className="p-4 text-center">Skills</th>
+                                <th className="p-4 text-center">About</th>
+                            </tr>
+                        </thead>
+
+                        {/* Table Body */}
+                        <tbody>
+                            {connectionData?.map(connection => (
+                                <ConnectionRequestCard key={connection._id} user={connection} />
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default Connections;
